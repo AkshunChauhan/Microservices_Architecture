11fe86669fa189c0089ca71a7f481c74fe3f3382
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-03-29 01:18:31 -0600

    initial commit

---
+++ src/main/java/com/example/Microservices_Architecture/MicroservicesArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservices_Architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Microservices_Architecture/MicroservicesArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microservices_Architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
